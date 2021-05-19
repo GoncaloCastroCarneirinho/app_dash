e33739c92f3dc940949232a1d8c6d2d110ab1d53
GoncaloCastroCarneirinho <goncalo2c@gmail.com>
2021-05-19 12:09:45 +0100

    Updating df.columns values filter...

--- dash_filtro_variables.py
+++ dash_filtro_variables.py
@@ -17,12 +17,12 @@ import plotly.express as px
 
 from pygraphtec import  lee_fichero_sesion
 
-df = lee_fichero_sesion("201112-165432.csv", path_sesiones='dataLogger')
+df = lee_fichero_sesion("201112-165432.csv", path_sesiones='/Users/scpgo/.spyder-py3/dataLogger')
 
 app = dash.Dash()
 
-#fig_names = ['fig1', 'fig2']
 fig_names = df.columns #asigno fig_names a las columnas del Dataframe
+
 cols_dropdown = html.Div([ #Div del filtro de variables
     dcc.Dropdown(
         id='cols_dropdown',
@@ -30,20 +30,22 @@ cols_dropdown = html.Div([ #Div del filtro de variables
         value=None, #ninguna opcion inicial preseleccionada    
         multi=True #permite selección de varias opciones
     )])
+
 fig_plot = html.Div(id='fig_plot') #Div de la gráfica
+
 app.layout = html.Div([cols_dropdown, fig_plot]) #permite construir la estructura el filtro
                                                  #de variables y la gráfica
 
 @app.callback( #permite devolver la gráfica como Dash Core Component dcc.Graph (línea 44)
 dash.dependencies.Output('fig_plot', 'children'),
-[dash.dependencies.Input('cols_dropdown', 'options')])
-def update_output(a):
-    return name_to_figure(a) #
+[dash.dependencies.Input('cols_dropdown', 'value')])
 
-def name_to_figure(a):
-    figure=px.line(df,y=fig_names) #se crea figura que representa todas las variables
+def name_to_figure(value):
+    if value is None or len(value)==0:
+        figure = {}
+    else:
+        figure=px.line(df[value]) #se crea figura que representa todas las variables
                                    #fig_names corresponde a la variable global (línea 25)
     return dcc.Graph(figure=figure)
-    #return [{'label': x, 'value': x} for x in fig_names]
 
 app.run_server(debug=True, use_reloader=False) #arranca la aplicacion
